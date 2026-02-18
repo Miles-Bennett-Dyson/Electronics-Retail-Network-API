e2bc2e883dd1a32d64fa1e19c78100445e36784c
Denis Gabdrakhmanov <rubik5000@ya.ru>
2026-02-18 15:36:54 +0500

    создано приложение и модель users

---
+++ users/models.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    username = None
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = "Учетная запись"
+        verbose_name_plural = "Учетные записи"
+        ordering = ["pk"]
