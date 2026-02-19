764b73aacef5c8bcbb3a61f3ff77ba7665c61fc3
Denis Gabdrakhmanov <rubik5000@ya.ru>
2026-02-19 17:19:53 +0500

    добавлены эндпоинты для получения токенов сотрудниками

--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("retail.urls", namespace="retail")),
+    path("", include("employees.urls", namespace="employees")),
 ]
